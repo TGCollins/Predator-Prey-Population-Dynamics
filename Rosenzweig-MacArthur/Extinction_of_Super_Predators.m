bf6fd6b67c6ff74e5b2d6e6c7c4c2f73a23da1ca
Toby Collins <106698921+TGCollins@users.noreply.github.com>
2022-12-22 11:56:34 +0000

    Update Extinction_of_Super_Predators.m

--- Rosenzweig-MacArthur/Extinction_of_Super_Predators.m
+++ Rosenzweig-MacArthur/Extinction_of_Super_Predators.m
@@ -41,30 +41,35 @@ tspan=linspace(0,tend,N+1)';
 % Plotting graph
 %===================================
 
-r0=12; w0=2;
-title_params={'r0','w0','beta','alphad','delta','alphaw'};
-plot(t,y(:,1), 'b',t,y(:,2),'r','LineWidth',2); grid on;
-xlabel('Time'), ylabel('Population Density');
-legend('D(t)','W(t)','location','best')
+plot(t,y(:,1),'g',t,y(:2),'b',t,y(:3),'r','LineWidth',1.2); grid on;
+xlabel('Time'), ylabel('Population Desnity');
+legend('E(t)','M(t)','F(t)','location','best')
 hold off
 
 %===================================
 % Function
 %===================================
 
-function dy = LotkaVolterra(t,y,params)
+function dy = RMModelv3(t,y,params)
   
-  beta = params(1);
-  alphad = params(2);
-  delta = params(3);
-  alphaw = params(4);
+  r1 = params(1);
+  a1 = params(2);
+  a2 = params(3);
+  g1 = params(4);
+  g2 = params(5);
+  d1 = params(6);
+  d2 = params(7);
 
-  D = y(1);
-  W = y(2);
+  K = 1.9; b1 = 0.6; b2 = 0.8;
+  
+  E = y(1);
+  M = y(2);
+  F = y(3)
+  
+  dy = zeros(3,1);
 
-  dy = zeros(2,1);
-
-  dy(1) = beta * D - alphad * D * W;
-  dy(2) = alphaw * D * W - delta * W;
+  dy(1) = (r.*(1-E./K) - (a1.*M)./(b1+E)).*E;
+  dy(2) = ((g1.*a1.*E)./(b1+E) - d1 - F./(b2+M)).*M;
+  dy(3) = ((g2.*a2.*E)./(b2+M) - d2).*F;
 
 end
